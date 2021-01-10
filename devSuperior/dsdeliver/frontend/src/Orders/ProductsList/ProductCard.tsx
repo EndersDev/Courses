a853e1c1ced0134d19d5f60faebeec78764fca5f
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-01-10 15:59:29 -0300

    fix name and image bug

--- devSuperior/dsdeliver/frontend/src/Orders/ProductsList/ProductCard.tsx
+++ devSuperior/dsdeliver/frontend/src/Orders/ProductsList/ProductCard.tsx
@@ -4,17 +4,27 @@ type Props = {
 	product: Product
 }
 
+function formatPrice(price: number) {
+	const formatter = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+		minimumFractionDigits: 2,
+	})
+
+	return formatter.format(price)
+}
+
 function ProductCard({ product }: Props) {
 	return (
 		<div className='product-card-container'>
-			<h3 className='product-card-title'>{product.title}</h3>
+			<h3 className='product-card-title'>{product.name}</h3>
 
 			<img
 				className='product-card-image'
-				src='{product.imageUri}'
+				src={product.imageUri}
 				alt='Produto'
 			/>
-			<h3 className='product-card-price'>{product.price}</h3>
+			<h3 className='product-card-price'>{formatPrice(product.price)}</h3>
 
 			<div className='product-card-description'>
 				<h3>Descrição</h3>
