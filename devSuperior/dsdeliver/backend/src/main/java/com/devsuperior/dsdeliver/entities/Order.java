f34d2a16e70aaa799f3bc3fa4feeb3ce1d454c10
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-01-11 06:11:34 -0300

    test: netfly watches just frontend dir 2

--- devSuperior/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ devSuperior/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-//teste netfly deploy
+//teste netfly deploy 2
   private static final long serialVersionUID = 1L;
 
   @Id
