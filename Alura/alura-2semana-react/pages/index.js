f8622020cd2fca4ba9f15d6836f4e39e3a0a5d06
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-01-31 01:10:55 -0300

    form reactive

--- Alura/alura-2semana-react/pages/index.js
+++ Alura/alura-2semana-react/pages/index.js
@@ -1,25 +1,19 @@
-import styled from 'styled-components'
 import db from '../db.json'
+import { useRouter } from 'next/router'
 
 import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
+import {
+  QuizLogo,
+  QuizBackground,
+  QuizContainer
+} from '../src/components/Quizzes'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-import StartForm from './../src/components/StartForm'
-
-const QuizContainer = styled.section`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
+import { Button, Input } from './../src/components/StartForm'
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState(db.player)
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -29,10 +23,32 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <h2>{db.description}</h2>
             <br />
             <br />
-            <StartForm />
+            <form
+              onSubmit={(infosDoEvento) => {
+                infosDoEvento.preventDefault()
+                router.push(`/quiz?name=${name}`)
+                //console.log('Fazendo uma submissão por meio do react')
+              }}
+            >
+              <Input
+                onChange={(infosDoEvento) => {
+                  //console.log(infosDoEvento.target.value)
+                  setName(infosDoEvento.target.value)
+                }}
+                placeholder="Insira seu Nome"
+                value={name}
+                name="Nome Do Usuario"
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                Play
+              </Button>
+              <br />
+              <br />
+              <label>Jogador: {name}</label>
+            </form>
           </Widget.Content>
         </Widget>
 
--- Alura/alura-2semana-react/src/components/StartForm.js
+++ Alura/alura-2semana-react/src/components/StartForm.js
@@ -1,29 +1,67 @@
-import { useRouter } from 'next/router'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+//INPUT
 
-export default function StartForm() {
-  const router = useRouter()
-  const [name, setName] = React.useState('Player1')
+const InputBase = styled.input`
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  outline: 0;
+  margin-bottom: 25px;
+`
 
+function Input({ onChange, placeholder, ...props }) {
   return (
-    <form
-      onSubmit={(infosDoEvento) => {
-        infosDoEvento.preventDefault()
-        router.push(`/quiz?name=${name}`)
-        console.log('Fazendo uma submissão por meio do react')
-      }}
-    >
-      <input
-        onChange={(infosDoEvento) => {
-          console.log(infosDoEvento.target.value)
-          setName(infosDoEvento.target.value)
-        }}
-        placeholder="Diz ai seu nome"
-      />
-      <button type="submit" disabled={name.length === 0}>
-        <h1>Play</h1>
-      </button>
-      <br />
-      <label>Jogador: {name}</label>
-    </form>
+    <div>
+      <InputBase placeholder={placeholder} onChange={onChange} {...props} />
+    </div>
   )
 }
+
+Input.defaultProps = {
+  value: ''
+}
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string
+}
+
+//BUTTON
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  border: 0;
+  width: 100%;
+  padding: 10px 16px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 1;
+  text-transform: uppercase;
+  outline: 0;
+  transition: 0.3s;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    opacity: 0.5;
+  }
+  &:disabled {
+    background-color: #979797;
+    cursor: not-allowed;
+  }
+`
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+  children: PropTypes.node.isRequired
+}
+
+export { Button, Input }
