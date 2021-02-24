28da9756f518f346870605e95b98c4131cb92af7
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-02-24 02:23:20 -0300

    back end upgrade

--- treinamentoVue3/backend/index.js
+++ treinamentoVue3/backend/index.js
@@ -1,4 +1,3 @@
-
 const Koa = require('koa')
 const Router = require('koa-router')
 const jwt = require('koa-jwt')
@@ -15,8 +14,8 @@ const app = new Koa()
 const router = new Router()
 
 const {
-  JWT_SECRET = process.env.JWT_SECRET_KEY,
-  PORT = 3030
+  JWT_SECRET = '',
+  PORT = 3000
 } = process.env
 const authMiddleware = jwt({ secret: JWT_SECRET })
 app.use(bodyParser())
